65b4aafb15e91537f99133381b22a1dae2a508f5
cuoicungtui <tuongdangvuongquoc901@gmail.com>
2022-06-24 17:02:05 +0700

    upload

---
+++ Third Person/Assets/Scrip/Arrow.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    public int damageAmount = 20;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, 10);  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(transform.GetComponent<Rigidbody>());
+
+        if (other.CompareTag("Dragon"))
+        {
+            transform.parent = other.transform;
+
+            other.GetComponent<dragon>().takeDamage(damageAmount);
+
+        }
+
+    }
+}
---
+++ Third Person/Assets/Scrip/dragon.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dragon : MonoBehaviour
+{
+    public int HP = 100;
+
+    public Animator animator;
+
+    public void takeDamage(int damageAmount)
+    {
+        HP -= damageAmount;
+        if(HP <= 0)
+        {
+            animator.SetTrigger("die");
+        }
+        else
+        {
+            animator.SetTrigger("damage");
+        }
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
